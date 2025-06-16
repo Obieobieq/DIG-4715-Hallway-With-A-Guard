1979f09ee4a7798b3fb4d3195e6bf5cbfe355bc9
Obieobieq <163595551+Obieobieq@users.noreply.github.com>
2025-06-16 14:24:10 -0400

    Pause Menu

--- Assets/Scripts/Scene Transition Scripts/ExitToMenu.cs
+++ Assets/Scripts/Scene Transition Scripts/ExitToMenu.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainToLevel : MonoBehaviour
+public class ExitToMenu : MonoBehaviour
 {
     void Awake()
     {
-        SceneManager.LoadScene("Level");
+        SceneManager.LoadScene("Menu");
     }
 }
--- Assets/Scripts/Scene Transition Scripts/Pausing/Pausing.cs
+++ Assets/Scripts/Scene Transition Scripts/Pausing/Pausing.cs
@@ -1,22 +1,52 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pausing : MonoBehaviour
 {
-    private bool isPaused = false;
+    public GameObject pauseMenu;
+
+    public static bool isPaused = false;
+
 
     void Update()
     {
-        if (Input.GetButtonDown("Pause") && isPaused == false)
+        if (Input.GetButtonDown("Pause"))
         {
-            Time.timeScale = 0;
-            isPaused = true;
-        }
-
-        if (Input.GetButtonDown("Pause") && isPaused == true)
-        {
-            Time.timeScale = 1;
-            isPaused = false;
+            if (isPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
         }
 
     }
+
+    public void Resume() 
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ReturnToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
